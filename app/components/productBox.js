ed57484a601c05aff0682bfd9dc66f2c44dc496f
Gezhen <gezhen_auto@163.com>
2017-03-21 14:24:32 +0800

    seed created

---
+++ app/components/productBox.js
@@ -0,0 +1,3 @@
+/**
+ * Created by hydra320 on 2017/3/21.
+ */
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './app/main.js')],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
+
+module.exports = config;
