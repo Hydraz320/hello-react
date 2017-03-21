22b3b94e17ca8418b59d6645185255b78b2015eb
Gezhen <gezhen_auto@163.com>
2017-03-21 14:55:59 +0800

    seed settled

--- app/main.js
+++ app/main.js
@@ -5,9 +5,9 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
-let AppComponent = require('./components/productBox.js');
+// let AppComponent = require('./components/productBox.js');
 
 ReactDOM.render(
-    <AppComponent />,
-    document.getElementById('content')
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
 );
