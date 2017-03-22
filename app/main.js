c189f53c66a452ea1b132991a63dc2c268976a09
Gezhen <gezhen_auto@163.com>
2017-03-22 15:45:29 +0800

    react学习ing

--- app/main.js
+++ app/main.js
@@ -7,7 +7,40 @@ let ReactDOM = require('react-dom');
 
 // let AppComponent = require('./components/productBox.js');
 
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timeID = setInterval(
+            ()=> this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timeID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <Clock/>,
     document.getElementById('root')
 );
